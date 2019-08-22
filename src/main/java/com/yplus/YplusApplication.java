584aef24ba1f73c8f381ba8a0658db33d0aa0a38
Your Name <tjk163@163.com>
2019-08-22 10:45:14 +0800

    day one initial

---
+++ src/main/java/com/yplus/YplusApplication.java
@@ -0,0 +1,13 @@
+package com.yplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YplusApplication.class, args);
+    }
+
+}
