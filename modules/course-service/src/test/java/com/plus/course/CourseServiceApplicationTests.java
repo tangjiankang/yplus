8d3913847624498058ef7278799a748a59fdd827
Your Name <tjk163@163.com>
2019-08-26 10:14:59 +0800

    add service

--- modules/course-service/src/test/java/com/plus/course/CourseServiceApplicationTests.java
+++ modules/course-service/src/test/java/com/plus/course/CourseServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yplus;
+package com.plus.course;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserServiceApplicationTests {
+public class CourseServiceApplicationTests {
 
     @Test
     public void contextLoads() {
--- modules/order-service/src/test/java/com/yplus/order/OrderServiceApplicationTests.java
+++ modules/order-service/src/test/java/com/yplus/order/OrderServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.plus;
+package com.yplus.order;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CourseServiceApplicationTests {
+public class OrderServiceApplicationTests {
 
     @Test
     public void contextLoads() {
